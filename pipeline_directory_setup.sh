c4720d733748838b025ae20641d3bf689c418d7a
João Raimundo <32943125+ray2g@users.noreply.github.com>
2020-11-05 16:44:43 +0000

    Update pipeline_directory_setup.sh

--- pipeline_directory_setup.sh
+++ pipeline_directory_setup.sh
@@ -3,4 +3,9 @@
 # turn it a executable file: $ sudo chmod +x pipeline_directory_setup.sh
 # execute it: $ ./pipeline_directory_setup.sh # to create the structured directory
 
+#script:
+
+mkdir vcall_biodata vcall_biodata/samples vcall_biodata/genome vcall_biodata/outputs vcall_biodata/outputs/logs
+wget https://raw.githubusercontent.com/ray2g/vcall_biodata/master/config_pipeline.yaml -O vcall_biodata/config_pipeline.yaml
+wget https://raw.githubusercontent.com/ray2g/vcall_biodata/master/vcall-pipe.snake -O vcall_biodata/vcall-pipe.snake
 
